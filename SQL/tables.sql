13938d2f5baffd40a74fd7e3367f8187e582e69c
Peto A <ak@Petos-MacBook-Pro.local>
2023-06-19 16:32:21 -0400

    added table stuff for jonny

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -1,11 +1,24 @@
 CREATE TABLE Cars (
+VIN,
+Year,
+Model,
+customerID
 
 );
 
 CREATE TABLE Customer (
-
+    PersonID int NOT NULL AUTO_INCREMENT,
+    ID,
+    Name,
+    Age,
+    Occupation,
+    Salary
 );
 
 CREATE TABLE Transactions (
-    
+    Transaction ID, 
+    Customer ID,
+    car ID,
+    Price,
+    Date of Purchase
 );
