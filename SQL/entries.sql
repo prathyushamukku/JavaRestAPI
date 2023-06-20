9ecbc8112f8961d12799a68a5735ee39975da421
Jonathon <jonathon.esbri@gmail.com>
2023-06-20 14:17:36 -0400

    fixed again

--- SQL/entries.sql
+++ SQL/entries.sql
@@ -1,18 +1,18 @@
 --Entries for the car information
-INSERT INTO Cars VALUES ('WVW7771J7YW248023','2003','Jetta');
-INSERT INTO Cars VALUES ('3VWNF81K37M133195','2007','Jetta');
-INSERT INTO Cars VALUES ('WVWSE61J23W310723','2003','Jetta');
-INSERT INTO Cars VALUES ('WVWMD63BXXE374070','1999','Passat');
-INSERT INTO Cars VALUES ('WVWBB7AJ2AW409084','2010','Golf');
+INSERT INTO Cars (VIN,MYear,Model) VALUES ('WVW7771J7YW248023','2003','Jetta');
+INSERT INTO Cars (VIN,MYear,Model) VALUES ('3VWNF81K37M133195','2007','Jetta');
+INSERT INTO Cars (VIN,MYear,Model) VALUES ('WVWSE61J23W310723','2003','Jetta');
+INSERT INTO Cars (VIN,MYear,Model) VALUES ('WVWMD63BXXE374070','1999','Passat');
+INSERT INTO Cars (VIN,MYear,Model) VALUES ('WVWBB7AJ2AW409084','2010','Golf');
 --Entries for the Customer information
-INSERT INTO Customers VALUES ('Jonny','55','Nerd','$0K');
-INSERT INTO Customers VALUES ('Nathan','38','Business Man','$8B');
-INSERT INTO Customers VALUES ('Drew','46','Chad','$275K');
-INSERT INTO Customers VALUES ('Akash','45','Finance man','$1M');
-INSERT INTO Customers VALUES ('Prathyusha','34','Chef','$1T');
+INSERT INTO Customers (Name,Age,Occupation, Salary) VALUES ('Jonny','55','Nerd','$0K');
+INSERT INTO Customers (Name,Age,Occupation, Salary) VALUES ('Nathan','38','Business Man','$8B');
+INSERT INTO Customers (Name,Age,Occupation, Salary) VALUES ('Drew','46','Chad','$275K');
+INSERT INTO Customers (Name,Age,Occupation, Salary) VALUES ('Akash','45','Finance man','$1M');
+INSERT INTO Customers (Name,Age,Occupation, Salary) VALUES ('Prathyusha','34','Chef','$1T');
 --Entries for the Transation information
-INSERT INTO Transactions VALUES ('2000','12-2-2020');
-INSERT INTO Transactions VALUES ('4500','2-5-2021');
-INSERT INTO Transactions VALUES ('1200','8-1-2019');
-INSERT INTO Transactions VALUES ('1000','6-10-2014');
-INSERT INTO Transactions VALUES ('12000','7-11-2023');
+INSERT INTO Transactions (Price,Dateofpurchase) VALUES ('2000','12-2-2020');
+INSERT INTO Transactions (Price,Dateofpurchase) VALUES ('4500','2-5-2021');
+INSERT INTO Transactions (Price,Dateofpurchase) VALUES ('1200','8-1-2019');
+INSERT INTO Transactions (Price,Dateofpurchase) VALUES ('1000','6-10-2014');
+INSERT INTO Transactions (Price,Dateofpurchase) VALUES ('12000','7-11-2023');
--- SQL/tables.sql
+++ SQL/tables.sql
@@ -1,46 +1,27 @@
--- Table for cars
+
 
 CREATE TABLE Cars (
-    --VIN for the vehicle
     VIN varchar(100),
-    --manufacture year
     MYear YEAR,
-    --Model for the car
     Model varchar(100),
-    --The vin is the primary key
     PRIMARY KEY (VIN)
 );
 
---Table for customer
 
-CREATE TABLE Customer (
-    --ID for the customer
+CREATE TABLE Customers (
     PersonID int NOT NULL AUTO_INCREMENT,
-    --name of customer
     Name varchar(100),
-    --age of customer
     Age varchar(100),
-    --Occupation of customer
     Occupation varchar(100),
-    --salary
     Salary varchar(100),
-    --the person ID is this tables  primary key
     PRIMARY KEY (PersonID)
 );
 
---Table for transactions
-
 CREATE TABLE Transactions (
-    --Transaction increment ID
     TransactionID int NOT NULL AUTO_INCREMENT,
-    --Customer ID
     PersonID varchar(100),
-    --Price for the vehicle
     Price int(100),
-    --The day that they purchased the vehicle
     Dateofpurchase date,
-    --Auto increment transaction ID
     PRIMARY KEY (TransactionID),
-    --PersonID is our secondary key
     FOREIGN KEY (PersonID)
 );
