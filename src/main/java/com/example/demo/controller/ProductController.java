a0a9c1564c867ec46c07a25a680b61940e17ab28
SaiPrathyusha <smukku1s@semo.edu>
2023-06-20 10:24:12 -0400

    Static Post and Get methods

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,25 +1,40 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.ProductDetails;
+import com.example.demo.model.ProductResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ProductController {
 
-    @GetMapping("/")
-    public String create_data() {
+    @GetMapping("/getData")
+    public List<ProductDetails> getAllProducts() {
+        List<ProductDetails> result = new ArrayList<>();
 
-        return "Hello All!";
+        result.add(new ProductDetails("234", "Durable", 8000, 7));
+        result.add(new ProductDetails("456", "Long lasting", 9000, 3));
+
+        return result;
 
     }
 
-    @PostMapping("/putData")
-    public String post_data(@RequestBody final ProductDetails productDetails) {
+    @PostMapping("/createData")
+    public ResponseEntity<ProductResponse> createData(@RequestBody final ProductDetails productDetails) {
         System.out.println(productDetails);
-        return "123456";
+        ProductResponse productResponse = new ProductResponse(productDetails.getPid());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(productResponse);
 
     }
 
--- src/main/java/com/example/demo/model/ProductResponse.java
+++ src/main/java/com/example/demo/model/ProductResponse.java
@@ -1,4 +1,19 @@
 package com.example.demo.model;
 
 public class ProductResponse {
+
+    private String pid;
+
+    public ProductResponse(String pid) {
+        this.pid = pid;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
 }
