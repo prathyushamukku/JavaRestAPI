8047602df90f5b35ca028c19cb0a7556319b9c7e
Jonathon <jonathon.esbri@gmail.com>
2023-06-20 13:57:25 -0400

    fixed insert statements

--- SQL/entries.sql
+++ SQL/entries.sql
@@ -1,18 +1,18 @@
 --Entries for the car information
-INSERT INTO Cars ('WVW7771J7YW248023','2003','Jetta');
-INSERT INTO Cars ('3VWNF81K37M133195','2007','Jetta');
-INSERT INTO Cars ('WVWSE61J23W310723','2003','Jetta');
-INSERT INTO Cars ('WVWMD63BXXE374070','1999','Passat');
-INSERT INTO Cars ('WVWBB7AJ2AW409084','2010','Golf');
+INSERT INTO Cars VALUES ('WVW7771J7YW248023','2003','Jetta');
+INSERT INTO Cars VALUES ('3VWNF81K37M133195','2007','Jetta');
+INSERT INTO Cars VALUES ('WVWSE61J23W310723','2003','Jetta');
+INSERT INTO Cars VALUES ('WVWMD63BXXE374070','1999','Passat');
+INSERT INTO Cars VALUES ('WVWBB7AJ2AW409084','2010','Golf');
 --Entries for the Customer information
-INSERT INTO Customers ('Jonny','55','Nerd','$0K');
-INSERT INTO Customers ('Nathan','38','Business Man','$8B');
-INSERT INTO Customers ('Drew','46','Chad','$275K');
-INSERT INTO Customers ('Akash','45','Finance Bro','$1M');
-INSERT INTO Customers ('Prathyusha','34','Chef','$1T');
+INSERT INTO Customers VALUES ('Jonny','55','Nerd','$0K');
+INSERT INTO Customers VALUES ('Nathan','38','Business Man','$8B');
+INSERT INTO Customers VALUES ('Drew','46','Chad','$275K');
+INSERT INTO Customers VALUES ('Akash','45','Finance man','$1M');
+INSERT INTO Customers VALUES ('Prathyusha','34','Chef','$1T');
 --Entries for the Transation information
-INSERT INTO Transactions ('2000','12-2-2020');
-INSERT INTO Transactions ('4500','2-5-2021');
-INSERT INTO Transactions ('1200','8-1-2019');
-INSERT INTO Transactions ('1000','6-10-2014');
-INSERT INTO Transactions ('12000','7-11-2023');
+INSERT INTO Transactions VALUES ('2000','12-2-2020');
+INSERT INTO Transactions VALUES ('4500','2-5-2021');
+INSERT INTO Transactions VALUES ('1200','8-1-2019');
+INSERT INTO Transactions VALUES ('1000','6-10-2014');
+INSERT INTO Transactions VALUES ('12000','7-11-2023');
