9a6c1922e630c4a5fc5f9c8e944053cbab8e5626
nathanm4 <nathan.mukku@gmail.com>
2023-06-20 14:31:51 -0400

    change customers to customer

--- SQL/entries.sql
+++ SQL/entries.sql
@@ -5,11 +5,11 @@ INSERT INTO Cars (VIN,MYear,Model) VALUES ('WVWSE61J23W310723','2003','Jetta');
 INSERT INTO Cars (VIN,MYear,Model) VALUES ('WVWMD63BXXE374070','1999','Passat');
 INSERT INTO Cars (VIN,MYear,Model) VALUES ('WVWBB7AJ2AW409084','2010','Golf');
 --Entries for the Customer information
-INSERT INTO Customers (Name,Age,Occupation, Salary) VALUES ('Jonny','55','Nerd','$0K');
-INSERT INTO Customers (Name,Age,Occupation, Salary) VALUES ('Nathan','38','Business Man','$8B');
-INSERT INTO Customers (Name,Age,Occupation, Salary) VALUES ('Drew','46','Chad','$275K');
-INSERT INTO Customers (Name,Age,Occupation, Salary) VALUES ('Akash','45','Finance man','$1M');
-INSERT INTO Customers (Name,Age,Occupation, Salary) VALUES ('Prathyusha','34','Chef','$1T');
+INSERT INTO Customer (Name,Age,Occupation, Salary) VALUES ('Jonny','55','Nerd','$0K');
+INSERT INTO Customer (Name,Age,Occupation, Salary) VALUES ('Nathan','38','Business Man','$8B');
+INSERT INTO Customer (Name,Age,Occupation, Salary) VALUES ('Drew','46','Chad','$275K');
+INSERT INTO Customer (Name,Age,Occupation, Salary) VALUES ('Akash','45','Finance man','$1M');
+INSERT INTO Customer (Name,Age,Occupation, Salary) VALUES ('Prathyusha','34','Chef','$1T');
 --Entries for the Transation information
 INSERT INTO Transactions (Price,Dateofpurchase) VALUES ('2000','12-2-2020');
 INSERT INTO Transactions (Price,Dateofpurchase) VALUES ('4500','2-5-2021');
