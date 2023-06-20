e0c8610ae4f247a26bef02422a96ca344c6323d4
Jonathon <jonathon.esbri@gmail.com>
2023-06-20 09:58:03 -0400

    commented tables

--- SQL/entries.sql
+++ SQL/entries.sql
@@ -1,13 +1,16 @@
+--Entries for the car information
 INSERT INTO Cars ('WVW7771J7YW248023','2003','Jetta');
 INSERT INTO Cars ('3VWNF81K37M133195','2007','Jetta');
 INSERT INTO Cars ('WVWSE61J23W310723','2003','Jetta');
 INSERT INTO Cars ('WVWMD63BXXE374070','1999','Passat');
 INSERT INTO Cars ('WVWBB7AJ2AW409084','2010','Golf');
-INSERT INTO Customers ('Jonny','Esbri','Nerd','$0K');
-INSERT INTO Customers ('Nathan','Mukku','Business Man','$8B');
-INSERT INTO Customers ('Drew','Oh','Chad','$275K');
-INSERT INTO Customers ('Akash','Naidu','Finance Bro','$1M');
-INSERT INTO Customers ('Prathyusha','Mukku','Chef','$1T');
+--Entries for the Customer information
+INSERT INTO Customers ('Jonny','55','Nerd','$0K');
+INSERT INTO Customers ('Nathan','38','Business Man','$8B');
+INSERT INTO Customers ('Drew','46','Chad','$275K');
+INSERT INTO Customers ('Akash','45','Finance Bro','$1M');
+INSERT INTO Customers ('Prathyusha','34','Chef','$1T');
+--Entries for the Transation information
 INSERT INTO Transactions ('2000','12-2-2020');
 INSERT INTO Transactions ('4500','2-5-2021');
 INSERT INTO Transactions ('1200','8-1-2019');
--- SQL/tables.sql
+++ SQL/tables.sql
@@ -1,24 +1,46 @@
+-- Table for cars
+
 CREATE TABLE Cars (
+    --VIN for the vehicle
     VIN varchar(100),
+    --manufacture year
     MYear YEAR,
+    --Model for the car
     Model varchar(100),
+    --The vin is the primary key
     PRIMARY KEY (VIN)
 );
 
+--Table for customer
+
 CREATE TABLE Customer (
+    --ID for the customer
     PersonID int NOT NULL AUTO_INCREMENT,
+    --name of customer
     Name varchar(100),
+    --age of customer
     Age varchar(100),
+    --Occupation of customer
     Occupation varchar(100),
+    --salary
     Salary varchar(100),
+    --the person ID is this tables  primary key
     PRIMARY KEY (PersonID)
 );
 
+--Table for transactions
+
 CREATE TABLE Transactions (
-    TransactionID int NOT NULL AUTO_INCREMENT, 
-    CustomerID varchar(100),
+    --Transaction increment ID
+    TransactionID int NOT NULL AUTO_INCREMENT,
+    --Customer ID
+    PersonID varchar(100),
+    --Price for the vehicle
     Price int(100),
-    dateofpurchase date,
+    --The day that they purchased the vehicle
+    Dateofpurchase date,
+    --Auto increment transaction ID
     PRIMARY KEY (TransactionID),
-    FOREIGN KEY (CustomerID)
+    --PersonID is our secondary key
+    FOREIGN KEY (PersonID)
 );
